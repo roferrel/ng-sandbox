84341833990fb381595f6f2a7692f4bf5b90828d
pat-hebert <patrick.hebert@cytel.com>
2025-01-06 14:28:59 -0800

    Initial commit

---
+++ src/example/card-fancy-example.ts
@@ -0,0 +1,20 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+/**
+ * @title Card with multiple sections
+ */
+@Component({
+  selector: 'card-fancy-example',
+  templateUrl: 'card-fancy-example.html',
+  styleUrl: 'card-fancy-example.css',
+  imports: [MatCardModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardFancyExample {}
+
+
+/**  Copyright 2025 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
